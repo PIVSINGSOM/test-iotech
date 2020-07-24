fb6060d5448d96c999253bb417bdcf3a0c43f551
Kraisorn086 <kraisorn.singsom@gmail.com>
2020-07-24 17:33:21 +0700

    create

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+      <div class="header-box">
+        Team Members
+      </div>
+      <div class="container">
+        <div class="box-filter">
+            <div class="clearfix">
+                <div class="box-input" >
+                  <input type="text" placeholder="Find By name" v-model="search">
+                </div>
+                <div class="box-count-item" >
+                  {{filteredDataObj.length}} People
+                </div>
+            </div>
+        </div>
+        <card-component v-for="(item,index) in filteredDataObj" :key="index" :info="item"></card-component>
+      </div>
+  </div>
+</template>
+
+<script>
+import CardComponent from '@/components/CardComponent'
+export default {
+  name: 'App',
+  components: {
+    'card-component':CardComponent
+  },
+  data() {
+    return {
+      listUser:[],
+      search:"",
+    }
+  },
+  computed:{
+    filteredDataObj() {
+        return this.listUser.filter((option) => {
+            return option.name
+              .toString()
+              .toLowerCase()
+              .indexOf(this.search.toLowerCase()) >= 0
+        })
+    }
+  },
+  mounted(){
+    this.getListUser();
+  },
+  methods:{
+    async getListUser(){
+      let response = await fetch('http://jsonplaceholder.typicode.com/users');
+      let result =  await response.json()
+      this.listUser = result
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <div class="card">
+      <div class="card-body">
+        <div class="clearfix">
+          <div class="float-left">
+            <div class="avatar inline"></div>
+            <div class="box-name-user inline">
+              <p>
+                <a class="user-name">{{ info.name }}</a>
+                <br />
+                <a class="user-email">{{ info.email }}</a>
+              </p>
+            </div>
+          </div>
+          <div class="float-right">
+            <p class="member">
+              Member
+              <i class="fa fa-chevron-right icon-member" @click="getInfoUser" v-if="!showInfo"></i>
+              <i class="fa fa-chevron-down icon-member" @click="getInfoUser" v-else></i>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="address" v-if="showInfo">
+          <strong>Address: </strong> {{newInfo.address.street}}, {{newInfo.address.suite}}, {{newInfo.address.city}}, {{newInfo.address.zipcode}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["info"],
+  data() {
+    return {
+      showInfo: false,
+      newInfo: {},
+      loading:false,
+    };
+  },
+  methods: {
+    async getInfoUser() {
+      if(this.showInfo == true){
+          this.showInfo = false
+          return
+      }
+      let response = await fetch(
+        `http://jsonplaceholder.typicode.com/users/${this.info.id}`
+      );
+      let result = await response.json();
+      this.newInfo = result
+      this.showInfo = true
+    },
+  },
+};
+</script>
+
+<style scoped>
+.user-name {
+  font-weight: 600;
+}
+.user-email {
+  font-size: 13px;
+  color: grey;
+  font-weight: 600;
+}
+.member {
+  margin-block-start: 5px;
+  color: grey;
+}
+.icon-member {
+  color: #05cd85;
+  cursor: pointer;
+}
+.address {
+  font-size: 13px;
+  color: grey;
+  /* height: 20px; */
+  padding: 5px;
+  background-color: #f8f8f8;
+  text-align: right;
+}
+</style>
