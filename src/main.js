7066ff6836a11e7f7874f42a009fb5d3a9448896
KraisornDev <kraisorn.singsom@gmail.com>
2022-01-25 00:12:49 +0700

    update

--- src/main.js
+++ src/main.js
@@ -4,8 +4,16 @@ import './assets/style.css'
 
 import Vue from 'vue'
 import App from './App.vue'
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false
+
+
+Vue.use(VueGtag, {
+  config: { id: "G-P3QHEKBCCV" }
+});
+
+
 
 new Vue({
   render: h => h(App),
